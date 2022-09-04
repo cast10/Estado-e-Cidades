cb78e5c9dfe4ebe3d461d2d809dd91668b497e2a
cast10 <castanha.douglas2013@gmail.com>
2022-09-04 18:17:49 -0300

    CRUD cidade Controller e Service feito

--- src/main/java/com/estadosecidades/Estados/e/cidade/Controller/CidadeController.java
+++ src/main/java/com/estadosecidades/Estados/e/cidade/Controller/CidadeController.java
@@ -1,39 +1,39 @@
 package com.estadosecidades.Estados.e.cidade.Controller;
 
 import com.estadosecidades.Estados.e.cidade.Model.CidadeModel;
-import com.estadosecidades.Estados.e.cidade.Model.EstadoModel;
-import org.apache.el.stream.Optional;
+import com.estadosecidades.Estados.e.cidade.Servide.CidadeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class CidadeController {
     @Autowired
-    private CidadeModel cidadeModel;
+    private CidadeService cidadeService;
 
     @GetMapping(path = "/cidade")
     public List<CidadeModel> listaCdade (){
-        return  null;
+        return cidadeService.buscarCidade();
 
     }@GetMapping(path = "/cidade/{id}")
     public Optional<CidadeModel> buscarCidade (@PathVariable long id){
-        return null;
+        return cidadeService.buscarCidadeId(id);
     }
 
     @PostMapping(path = "/cidade")
     public CidadeModel cadastrarCidade(@RequestBody CidadeModel cidadeModel    ){
-        return null;
+        return cidadeService.cadastrarCidade(cidadeModel);
     }
 
     @PutMapping(path = "/cidade /{id}")
     public CidadeModel slterarNomeCidade (@RequestBody CidadeModel cidadeModel){
-        return null;
+        return cidadeService.alterarNomeCidade(cidadeModel);
 
     }
     @DeleteMapping(path = "/cidade /{id}")
     public void deletarCidade(@PathVariable long id){
-        contaAPagarService.deleteId(id);
+        cidadeService.deleteCidadeID(id);
     }
 }
--- src/main/java/com/estadosecidades/Estados/e/cidade/Servide/CidadeService.java
+++ src/main/java/com/estadosecidades/Estados/e/cidade/Servide/CidadeService.java
@@ -1,4 +1,41 @@
 package com.estadosecidades.Estados.e.cidade.Servide;
 
+import com.estadosecidades.Estados.e.cidade.Model.CidadeModel;
+import com.estadosecidades.Estados.e.cidade.Repository.CidadeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class CidadeService {
+
+    @Autowired
+    private CidadeRepository cidadeRepository;
+
+    public List<CidadeModel> buscarCidade (){
+        return cidadeRepository.findAll();
+    }
+    public Optional<CidadeModel> buscarCidadeId (Long id){
+        return cidadeRepository.findById(id);
+    }
+    public CidadeModel cadastrarCidade (CidadeModel cidadeModel){
+
+        cidadeModel.getId();
+        cidadeModel.getNomeCidade();
+
+        return cidadeRepository.save(cidadeModel);
+    }
+    public CidadeModel alterarNomeCidade (CidadeModel cidadeModel){
+
+        cidadeModel.getId();
+        cidadeModel.getNomeCidade();
+
+        return cidadeRepository.save(cidadeModel);
+    }
+    public void deleteCidadeID(Long id){
+        cidadeRepository.deleteById(id);
+    }
+
 }
